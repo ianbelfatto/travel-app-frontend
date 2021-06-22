789a465aa5a9fe0292b586f20c9c3200e2a03bdb
ianbelfatto <ibelfatto@gmail.com>
2021-06-22 17:00:13 -0400

    creates vue authentication and begins vue crud

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
+import UsersShow from "../views/Users/UserShow.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
+  { path: "/", name: "Home", component: Home },
   {
     path: "/about",
-    name: "About",
-    // route level code-splitting
+    name: "About", // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
+  { path: "/users/:id", name: "users-show", component: "UsersShow" },
 ];
 
 const router = new VueRouter({
---
+++ src/views/Users/UserShow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="users-show">
+    <h2>{{ user.name }}</h2>
+    <img v-bind:src="user.url" v-bind:alt="user.name" />
+    <p>Width: {{ user.width }}</p>
+    <p>Height: {{ user.height }}</p>
+    <router-link to="/users">Back to all users</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      user: {},
+    };
+  },
+  created: function () {
+    axios.get("/users/" + this.$route.params.id).then((response) => {
+      console.log("users show", response);
+      this.user = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
