d356330de25205f418bc9b405aca1c3dc8df0660
ianbelfatto <ibelfatto@gmail.com>
2021-06-25 11:02:51 -0400

    adds trip index page

---
+++ src/views/Trips/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="trips-index">
+    <div v-for="trip in trips" v-bind:key="trip.id">
+      <h2>{{ trip.name }}</h2>
+      <img :src="trip.image_url" alt="" />
+      <p>City: {{ trip.city }}</p>
+      <p>State: {{ trip.state }}</p>
+      <router-link :to="`/trips/${trip.id}`">More Info</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      trips: [],
+    };
+  },
+  created: function () {
+    axios.get("/trips").then((response) => {
+      console.log("Trips array", response.data);
+      this.trips = response.data;
+    });
+  },
+};
+</script>
