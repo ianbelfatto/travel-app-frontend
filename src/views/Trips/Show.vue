f12c807ce4f206eec463b81dfff49c2a16d793c3
ianbelfatto <ibelfatto@gmail.com>
2021-06-25 11:03:15 -0400

    begins adding trip show page

---
+++ src/views/Trips/Show.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="trips-show">
+    <h2>{{ trip.name }}</h2>
+    <img :src="trip.image_url" alt="" />
+    <p>{{ trip.city }}</p>
+    <p>{{ trip.state }}</p>
+    <div v-for="trip_business in trip_businesses" v-bind:key="trip_business.id">
+      <p>Restaurants: {{ trip.trip_businesses[0].business }}</p>
+      <p>Restaurants: {{ trip.trip_business.name }}</p>
+    </div>
+    <router-link to="/trips">All Trips</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      trip: { user: {} },
+    };
+  },
+  created: function () {
+    axios.get(`/trips/${this.$route.params.id}`).then((response) => {
+      console.log("Trip object", response.data);
+      this.trip = response.data;
+    });
+  },
+};
+</script>
