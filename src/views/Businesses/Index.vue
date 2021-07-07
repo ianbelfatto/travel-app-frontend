42f51836182753a426db6937313be33ee0407ccd
ianbelfatto <ibelfatto@gmail.com>
2021-07-07 15:31:47 -0400

    adds Show modal on Business Index

--- src/views/Businesses/Index.vue
+++ src/views/Businesses/Index.vue
@@ -17,6 +17,32 @@
       <br />
       <br />
       <router-link tag="button" :to="`/businesses/${business.id}`">More Info</router-link>
+      <button v-on:click="showBusiness(business)" name="modal">Modal</button>
+      <dialog id="business-details">
+        <form method="dialog">
+          <h1>{{ currentBusiness.name }}</h1>
+          <img :src="currentBusiness.image_url" alt="" />
+          <p>Open: {{ currentBusiness.is_closed }}</p>
+          <p>Rating: {{ currentBusiness.rating }} / 5</p>
+          <p>Review Count: {{ currentBusiness.review_count }}</p>
+          <p>Cost: {{ currentBusiness.price }}</p>
+          <p>Phone Number: {{ currentBusiness.display_phone || "None Listed" }}</p>
+          <!-- <p>
+            {{ currentBusiness.location.display_address[0] + "," + " " + currentBusiness.location.display_address[1] }}
+          </p> -->
+          <br />
+          <br />
+          <h3>My Trips</h3>
+          <div v-for="trip in trips" v-bind:key="trip.id">
+            <i>{{ trip.name }}</i>
+            <br />
+            <button @click="addBusinessToTrip(trip)">Add to This Trip</button>
+            <br />
+            <br />
+          </div>
+          <button>Close</button>
+        </form>
+      </dialog>
       <br />
       <br />
     </div>
@@ -29,9 +55,22 @@ export default {
   data: function () {
     return {
       businesses: [],
+      currentBusiness: {},
       term: "",
       location: "",
     };
+  },
+  created: function () {
+    axios.all([
+      // axios.get(`/businesses/${this.$route.params.yelp_business_id}`).then((response) => {
+      //   console.log("business object", response.data);
+      //   this.business = response.data;
+      // }),
+      axios.get("/trips").then((response) => {
+        console.log("Trips array", response.data);
+        this.trips = response.data;
+      }),
+    ]);
   },
   methods: {
     runSearch: function () {
@@ -39,6 +78,33 @@ export default {
         console.log("Businesses array", response.data);
         this.businesses = response.data;
       });
+    },
+    showBusiness: function (business) {
+      console.log("current business", business);
+      this.currentBusiness = business;
+      document.querySelector("#business-details").showModal();
+    },
+    addBusinessToTrip: function (trip) {
+      const formData = new FormData();
+      formData.append("trip_id", trip.id);
+      formData.append("yelp_business_id", this.currentBusiness.id);
+      axios
+        .post("/tripbusiness", formData)
+        .then((response) => {
+          console.log(response.data);
+          this.$notify({ type: "success", text: "Business has been added to the trip!" });
+          // setTimeout(() => {
+          //   this.$router.push(`/trips/${trip.id}`);
+          // }, 2000);
+        })
+        .catch((error) => {
+          this.$notify({
+            type: "error",
+            text: "Something went wrong...Business already exists in this trip.",
+            title: error,
+          });
+          this.errors = error.response.data.errors;
+        });
     },
   },
 };
