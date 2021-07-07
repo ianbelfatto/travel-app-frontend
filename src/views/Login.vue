02915d113d56aba415a7975ffa22532b01b11087
ianbelfatto <ibelfatto@gmail.com>
2021-07-07 15:31:59 -0400

    changes Login push

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -41,7 +41,7 @@ export default {
           axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
           localStorage.setItem("id", response.data.user_id);
-          this.$router.push("/");
+          this.$router.push("/trips/mytrips");
           this.$notify({ type: "success", text: "You have been logged in." });
         })
         .catch((error) => {
