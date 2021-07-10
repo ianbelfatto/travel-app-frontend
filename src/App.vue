3522432c7e589011084f4100924183b235ae883c
ianbelfatto <ibelfatto@gmail.com>
2021-07-10 14:12:50 -0400

    comments out built in preloader

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <div id="app">
-    <!-- Preloader -->
-    <div id="preloader" class="smooth-loader-wrapper">
-      <div class="smooth-loader">
-        <div class="loader1">
-          <div class="loader-target">
-            <div class="loader-target-main"></div>
-            <div class="loader-target-inner"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+
 
     <div class="main-wrapper">
       <!-- HEADER -->
