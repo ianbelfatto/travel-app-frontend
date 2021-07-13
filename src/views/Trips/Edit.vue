7e8da44c775befe38b06fd46faa7d41b44f16e41
ianbelfatto <ibelfatto@gmail.com>
2021-07-13 16:01:48 -0400

    adds bootstrap loading to trip edit page

--- src/views/Trips/Edit.vue
+++ src/views/Trips/Edit.vue
@@ -1,51 +1,69 @@
 <template>
   <div class="trips-edit">
-    <!-- EDIT TRIP   -->
     <section class="py-7 py-md-10 bg-light">
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-6 col-lg-5">
-            <div class="card">
-              <div class="bg-primary text-center py-4">
-                <h2 class="text-white mb-0 h4">Edit Trip</h2>
+            <div class="container-fluid">
+              <div class="d-flex justify-content-center">
+                <div
+                  class="spinner-border text-primary"
+                  flex-column
+                  align-items-center
+                  style="width: 5rem; height: 5rem"
+                  role="status"
+                  v-if="isLoading"
+                ></div>
               </div>
-              <div class="card-body px-7 pt-7 pb-0">
-                <form v-on:submit.prevent="editTrip()">
-                  <ul>
-                    <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-                  </ul>
-                  <div class="form-group mb-7">
-                    <label for="exampleInputText">Name*</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      aria-describedby="emailHelp"
-                      required
-                      v-model="editTripParams.name"
-                    />
-                    <em>Enter a new name for this trip.</em>
-                  </div>
-                  <div class="form-group mb-7">
-                    <label for="exampleInputPassword1">City*</label>
-                    <input type="text" class="form-control" v-model="editTripParams.city" />
-                    <em>Enter a new city for this trip.</em>
-                  </div>
-                  <div class="form-group mb-7">
-                    <label for="exampleInputPassword1">State*</label>
-                    <input type="text" class="form-control" v-model="editTripParams.state" />
-                    <em>Enter a new state for this trip.</em>
-                  </div>
-                  <div class="form-group mb-7">
-                    <label for="exampleInputPassword1">Cover Image*</label>
-                    <input type="text" class="form-control" v-model="editTripParams.image_url" />
-                    <em>Enter a new Cover Image URL for this trip.</em>
-                  </div>
-                  <div class="form-group d-flex justify-content-between align-items-center mb-7">
-                    <router-link :to="`/trips/mytrips`" class="btn btn-secondary">Back to My Trips</router-link>
-                    &nbsp;
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </div>
-                </form>
+              <div v-if="isLoading">
+                <br />
+                <b><p style="text-align: center">Loading your content, please wait...</p></b>
+              </div>
+            </div>
+
+            <div v-if="!isLoading">
+              <div class="card">
+                <div class="bg-primary text-center py-4">
+                  <h2 class="text-white mb-0 h4">Edit Trip</h2>
+                </div>
+                <div class="card-body px-7 pt-7 pb-0">
+                  <form v-on:submit.prevent="editTrip()">
+                    <ul>
+                      <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+                    </ul>
+                    <div class="form-group mb-7">
+                      <label for="exampleInputText">Name*</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        aria-describedby="emailHelp"
+                        required
+                        v-model="editTripParams.name"
+                      />
+                      <em>Enter a new name for this trip.</em>
+                    </div>
+                    <div class="form-group mb-7">
+                      <label for="exampleInputPassword1">City*</label>
+                      <input type="text" class="form-control" v-model="editTripParams.city" />
+                      <em>Enter a new city for this trip.</em>
+                    </div>
+                    <div class="form-group mb-7">
+                      <label for="exampleInputPassword1">State*</label>
+                      <input type="text" class="form-control" v-model="editTripParams.state" />
+                      <em>Enter a new state for this trip.</em>
+                    </div>
+                    <div class="form-group mb-7">
+                      <label for="exampleInputPassword1">Cover Image*</label>
+                      <input type="text" class="form-control" v-model="editTripParams.image_url" />
+                      <em>Enter a new Cover Image URL for this trip.</em>
+                    </div>
+                    <div class="form-group d-flex justify-content-between align-items-center mb-7">
+                      <router-link :to="`/trips/mytrips`" class="btn btn-secondary">Back to My Trips</router-link>
+                      &nbsp;
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
@@ -62,6 +80,7 @@ export default {
     return {
       editTripParams: {},
       errors: [],
+      isLoading: true,
     };
   },
   created: function () {
@@ -69,6 +88,8 @@ export default {
       console.log("Trips Object", response.data);
       this.editTripParams = response.data;
       console.log(this.editTripParams);
+
+      this.isLoading = false;
     });
   },
   methods: {
@@ -78,7 +99,7 @@ export default {
         .then((response) => {
           console.log(response.data);
           this.$notify({ type: "success", text: "Successfully edited the Trip!" });
-          this.$router.push(`/trips/${response.data.id}`);
+          this.$router.push(`/trips/mytrips`);
         })
         .catch((error) => {
           this.$notify({ type: "error", text: "Something went wrong...", title: error });
